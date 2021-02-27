1f689ca29823fe87f70e25399ee10d502cef2876
wowonline <31239690+wowonline@users.noreply.github.com>
2021-02-27 16:05:56 +0300

    Update stock_price_plot.py

--- stock_price_plot.py
+++ stock_price_plot.py
@@ -1,11 +1,7 @@
 from matplotlib import pyplot as plt
-import yfinance as yf
 
 def stock_plot(x_axis : list, y_axis : list):
     fig, ax = plt.subplots()
     p = plt.plot(x_axis, y_axis)
     plt.savefig("plot.png")
     
-
-
-stock_plot(['1','2'], ['3', '80'])
