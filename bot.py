3c4d7f70a8d35491af5dd20545f31c26b370b704
Михаил Исаков <maisakov@miem.hse.ru>
2021-02-27 03:13:31 +0300

    3 version

--- bot.py
+++ bot.py
@@ -1,58 +1,123 @@
 import config
-import Keyboards
 import telebot
 import check_answers
 import time
 import finance
-
+from telebot import types
 bot = telebot.TeleBot(config.token)
 answer = check_answers.Answers()
+company = ""
+""" Инициализация начальной клавиатуры """
+keyboard_primary = types.InlineKeyboardMarkup()
+button_theory = types.InlineKeyboardButton(text = "Теория", callback_data = "Теория")
+button_quote = types.InlineKeyboardButton(text = "Получение одной котировки", callback_data = "Котировка")
+button_notification = types.InlineKeyboardButton(text = "Настройка уведомлений", callback_data = "Уведомление")
+keyboard_primary.add(button_theory, button_quote, button_notification)
+""" Инициализация клавиатуры теории """ 
+keyboard_theory = types.InlineKeyboardMarkup()
+button_investment = types.InlineKeyboardButton(text = "Инвестирование", callback_data = "Инвестирование")
+button_trading = types.InlineKeyboardButton(text = "Трейдинг", callback_data = "Трейдинг")
+keyboard_theory.add(button_investment, button_trading)
+""" Инициализация клавиатуры котировки """
+keyboard_quote = types.InlineKeyboardMarkup()
+button_ruble = types.InlineKeyboardButton(text = "В рублях", callback_data = "Рубль")
+button_currency = types.InlineKeyboardButton(text = "В исходной валюте", callback_data = "Валюта")
+keyboard_quote.add(button_ruble, button_currency)
+""" Инициализация клавиатуры уведомлени """
+keyboard_notification = types.InlineKeyboardMarkup()
+button_day = types.InlineKeyboardButton(text = "Раз в день", callback_data = "День")
+button_2days = types.InlineKeyboardButton(text = "Раз в два дня", callback_data = "2Дня")
+button_week = types.InlineKeyboardButton(text = "Раз в неделю", callback_data = "Неделя")
+keyboard_notification.add(button_day, button_2days, button_week)
 
+""" Отправка начальной клавиатуры """
+@bot.message_handler(commands=['start'])
+def create_inline_keyboard(message):
+    bot.send_message(message.chat.id, text = "Выберите опцию", reply_markup=keyboard_primary)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Подсоеденен пользователь, {message.from_user.first_name}')
-
-@bot.message_handler(commands=['set'])
-def generate_markup(message):
-    objMarkup = Keyboards.CostomizeKeyboard()
-    markup = objMarkup.generate_primary_markup()
-    bot.send_message(message.chat.id,'Выберите функции',reply_markup=markup)
-
-
-@bot.message_handler(commands=['reset'])
-def canceling_notifications(message):
-    bot.send_message(message.chat.id,"Вы отменили посылку уведомлений")
-    answer.existence_of_polling = 0
-    
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    hendler_messages(call.message, call.data)
 
 @bot.message_handler(content_types = ['text'])
-def hendler_messages(message):
+def hendler_messages(message, reply = None):
+    if reply == "Теория": 
+        bot.send_message(message.chat.id, text = "Выберите теорию", reply_markup = keyboard_theory)
+        bot.delete_message(message.chat.id, message.id)
 
-    if message.text == 'Hi':
-        bot.reply_to(message,f'Hello, {message.from_user.first_name}')
+    if reply == "Котировка":
+        bot.send_message(message.chat.id, text = "Выберите валюту", reply_markup = keyboard_quote)
+        bot.delete_message(message.chat.id, message.id)
 
-    elif message.text in config.PRIMARYKEYBOARD:
-        markup, text = answer.check_primary_answer(message.text)
-        bot.send_message(message.chat.id, text = text,reply_markup = markup)     
-       
-    elif message.text in config.THEORYKEYBOARD:
-        bot.reply_to(message, answer.check_theory_answer(message.text))
+    if reply == "Уведомление":
+        bot.send_message(message.chat.id, text = "Выбирите периодичность", reply_markup = keyboard_notification)
+        bot.delete_message(message.chat.id, message.id)
+
+    """ Отправка информации по теории """
+
+    if reply == "Инвестирование":
+        bot.send_message(message.chat.id, text = answer.theory_answer_investment())
+        bot.delete_message(message.chat.id, message.id)
+
+    if reply == "Трединг":
+        bot.send_message(message.chat.id, text = answer.theory_answer_trading())
+        bot.delete_message(message.chat.id, message.id)
+
+    """ Отправка информации по котеровки """
+
+    if reply == "Рубль":
+        bot.send_message(message.chat.id, text = answer.quote_answer_ruble(company))
+        bot.delete_message(message.chat.id, message.id)
+
+    if reply == "Валюта":
+        bot.send_message(message.chat.id, text = answer.quote_answer_currency(company))
+        bot.delete_message(message.chat.id, message.id)
+
+    """ Отправка уведомлений котеровок """
+
+    if reply == "День":
+        if answer.existence_of_polling == 0:
+            answer.existence_of_polling = 1
+            bot.delete_message(message.chat.id, message.id)
+            while answer.existence_of_polling == 1:
+                bot.send_message(message.chat.id, text = answer.notification_answer_day(company))
+                time.sleep(10)
+        else: 
+            bot.delete_message(message.chat.id, message.id)
+            bot.send_message(message.chat.id, text = "Уведомление выбрано")
+
+
+    if reply == "2Дня":
+        if answer.existence_of_polling == 0:
+            answer.existence_of_polling = 1
+            bot.delete_message(message.chat.id, message.id)
+            while answer.existence_of_polling == 1:
+                bot.send_message(message.chat.id, text = answer.notification_answer_2day(company))
+                time.sleep(20)
+        else:
+            bot.delete_message(message.chat.id, message.id)
+            bot.send_message(message.chat.id, text = "Уведомление выбрано")
+
+
+    if reply == "Неделя":
+        if answer.existence_of_polling == 0:
+            answer.existence_of_polling = 1
+            bot.delete_message(message.chat.id, message.id)
+            while answer.existence_of_polling == 1:
+                bot.send_message(message.chat.id, text = answer.notification_answer_week(company))
+                time.sleep(30)
+        else:
+            bot.delete_message(message.chat.id, message.id)
+            bot.send_message(message.chat.id, text = "Уведомление выбрано")
+
+    if message.text == "О":
+        print("j")
+        answer.existence_of_polling = 0
     
-    elif message.text in config.QUOTEKEYBOARD:
-        bot.reply_to(message, "Введите название компании ")
 
-    elif message.text == config.NOTIFICATIONKEYBOARD[0]:
-        answer.existence_of_polling = 1
-        while answer.existence_of_polling == 1:
-            bot.reply_to(message, answer.check_notification_answer(message.text,'AAPL'))
-            
-    
-        
-    else:
-        bot.send_message(message.chat.id,answer.check_quote_answer(message.text))
-
-
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Подсоеденен пользователь, {message.from_user.first_name}')
 
 
 if __name__ == '__main__':
--- check_answers.py
+++ check_answers.py
@@ -1,62 +1,37 @@
 import config
 import time
 import telebot
-import Keyboards
 import finance
 
 class Answers:
     def __init__(self):
-        self.list_of_notifications = [0,0,0]
+        #self.list_of_notifications = [0,0,0]
         self.existence_of_polling = 0
-        self.storage = finance.Storage()
-        self.storage.upload_storage()
-    #@classmethod    
-    def check_primary_answer(self,text):
-        print('Производится проверка первоначальных настроек')
-        if text == config.PRIMARYKEYBOARD[0]:
-            objMarkup = Keyboards.CostomizeKeyboard()
-            return objMarkup.generate_theory_markup(), 'Выбор темы'
+        #self.storage = finance.Storage()
+        #self.storage.upload_storage()
 
-        if text == config.PRIMARYKEYBOARD[1]:
-            objMarkup = Keyboards.CostomizeKeyboard()
-            return objMarkup.generate_quote_markup(), 'Выбор валюты'
-
-        if text == config.PRIMARYKEYBOARD[2]:
-            if self.existence_of_polling == 1:
-                return None, 'Вы уже выбрали показ уведомлений'
-            else:
-                objMarkup = Keyboards.CostomizeKeyboard()
-                return objMarkup.generate_notification_markup(), 'Выбор периода уведомлений' 
-
-    #@classmethod
-    def check_theory_answer(self,text):
-        if text == config.THEORYKEYBOARD[0]:
-            return 'Здесь будет информация про инвестирование'
+    def theory_answer_investment(self):
+        return 'Здесь будет информация про инвестирование'
         
-        if text == config.THEORYKEYBOARD[1]:
-            return 'Здесь будет информация про трейдинг'
+    def theory_answer_trading(self):
+        return 'Здесь будет информация про трейдинг'
 
-    #@classmethod
-    def check_quote_answer(self,company):
-        #if text == config_clone.QUOTEKEYBOARD[0]:
-        return self.storage.get_companies_data(company)
+    def quote_answer_ruble(self,company):
+        return 'Рубли'
+    
+    def quote_answer_currency(self,company):
+        return 'Текущее'
         
-        #if text == config_clone.QUOTEKEYBOARD[1]:
-            #return 'Здесь будет выводиться значение в рублях'
 
     #@classmethod
-    def check_notification_answer(self,text,company):
-        if text == config.NOTIFICATIONKEYBOARD[0]:
-            time.sleep(10)
-            return f'Компания: {company} стоит {self.storage.get_companies_data(company)}'
+    def notification_answer_day(self,company):
+            return f'Компания: {company} стоит '
                 
-        if text == config.NOTIFICATIONKEYBOARD[1]:
-            time.sleep(20)
-            return f'Компания: {company} стоит {self.storage.get_companies_data(company)}'
+    def notification_answer_2day(self,company):
+        return f'Компания: {company} стоит '
         
-        if text == config.NOTIFICATIONKEYBOARD[2]:
-            time.sleep(30)
-            return f'Компания: {company} стоит {self.storage.get_companies_data(company)}'
+    def notification_answer_week(self,company):
+        return f'Компания: {company} стоит '
 
     
 
